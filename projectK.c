730588ba3a83137107414821281b73be0d9119e5
Kevinrwh <50501047+Kevinrwh@users.noreply.github.com>
2020-04-15 15:19:57 -0400

    Create projectK.c

---
+++ projectK.c
@@ -0,0 +1,92 @@
+#include "spimcore.h"
+
+
+/* ALU */
+/* 10 Points */
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* instruction fetch */
+/* 10 Points */
+
+//  Pre-processor directives: This function takes in PC from Mem and stores it in instruction.
+//  Post-processor directives: function returns 1 if halt condition is met. Otherwise, return 0.
+int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
+{
+  //  Check if PC is word-aligned (divisible by 4) or beyond the scope of the memory of 64 kB.
+  if(PC%4 != 0 || PC > 65536)
+  {
+    return 1;
+  }
+  else
+  {
+    //  Get the instruction from Mem
+    *instruction = Mem[PC>>2];
+    return 0;
+  }
+    
+}
+
+
+/* instruction partition */
+/* 10 Points */
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+{
+
+}
+
+
+
+/* instruction decode */
+/* 15 Points */
+int instruction_decode(unsigned op,struct_controls *controls)
+{
+
+}
+
+/* Read Register */
+/* 5 Points */
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+
+}
+
+
+/* Sign Extend */
+/* 10 Points */
+void sign_extend(unsigned offset,unsigned *extended_value)
+{
+
+}
+
+/* ALU operations */
+/* 10 Points */
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* Read / Write Memory */
+/* 10 Points */
+int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+}
+
+
+/* Write Register */
+/* 10 Points */
+void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+{
+
+}
+
+/* PC update */
+/* 10 Points */
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
+
